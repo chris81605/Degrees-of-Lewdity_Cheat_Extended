18e8a5288b32369506f1fa9869556e1b397a6b2e
chris81605 <89143549+chris81605@users.noreply.github.com>
2024-10-20 17:43:25 +0800

    Add files via upload

--- scriptEarly/CERegist.js
+++ scriptEarly/CERegist.js
@@ -2,4 +2,5 @@ simpleFrameworks.addto('iModCheats', 'cheat_extended')
 simpleFrameworks.addto('ModMenuBig', 'bccmeditor_button')
 simpleFrameworks.addto('ModCaptionAfterDescription', 'bccmeditor_init')
 simpleFrameworks.addto('ModCaptionAfterDescription', 'CEstatebox')
+simpleFrameworks.addto('iModOptions', 'swich_DEBUG_MODE')
 
